6fe24f12930134141adfeb9bed717f4ab3500959
Mohebullah Mir <mohebullahmir@gmail.com>
2022-04-23 17:15:41 -0400

    Now ignores all spaces and punctuation

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ Once we've shifted the char we convert the ascii code back to a char.
 function caesarCipher(phrase,shift){
   let result="";
   for(let i=0;i<phrase.length;i++){
-    if (phrase.charAt(i)==" "){
-      result+=" ";
+    //check to see if char is a letter, if not then don't shift char.
+    if (phrase.charAt(i).toLowerCase()===phrase.charAt(i).toUpperCase()){
+      result+=phrase.charAt(i);
       continue;
     }
     let currentChar = phrase.charCodeAt(i);
